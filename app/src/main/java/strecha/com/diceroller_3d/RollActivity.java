82e1da0d43a0acaf3d83ccf4b8c0ed4c34f4eed4
Lukas Streckeisen <lukas.streckeisen@hispeed.ch>
2017-09-11 13:19:05 +0200

    added onClick method for rollActivity menu buttons

--- app/src/main/java/strecha/com/diceroller_3d/RollActivity.java
+++ app/src/main/java/strecha/com/diceroller_3d/RollActivity.java
@@ -1,7 +1,11 @@
 package strecha.com.diceroller_3d;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Selection;
+import android.view.View;
+import android.widget.Button;
 
 public class RollActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class RollActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll);
+    }
+
+
+    public void onMenuButtonClick(View v){
+        Intent intent = null;
+        if (v.getId() == R.id.butSelection){
+            intent = new Intent(this, SelectionActivity.class);
+        }
+        else if (v.getId() == R.id.butSettings){
+            intent = new Intent(this, SettingsActivity.class);
+        }
+        else if (v.getId() == R.id.butHistory){
+            intent = new Intent(this, HistoryActivity.class);
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
