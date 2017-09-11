4ea84e9fbf180307b92ad35bbd8be9f490bc0f91
Lukas Streckeisen <lukas.streckeisen@hispeed.ch>
2017-09-11 14:19:59 +0200

    added dice properties to rollActivity

--- app/src/main/java/strecha/com/diceroller_3d/RollActivity.java
+++ app/src/main/java/strecha/com/diceroller_3d/RollActivity.java
@@ -7,12 +7,31 @@ import android.text.Selection;
 import android.view.View;
 import android.widget.Button;
 
+import strecha.com.diceroller_3d.module.DiceType;
+
 public class RollActivity extends AppCompatActivity {
+
+    private static final String EXTRA_DICE_TYPE = "strecha.com.diceroller_3d.RollActivity.DiceType";
+    private static final String EXTRA_DICE_NUMBER = "strecha.com.diceroller_3d.RollActivity.DiceNumber";
+
+    private DiceType diceType = DiceType.D4;
+    private int diceNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll);
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra(EXTRA_DICE_TYPE)){
+            String s = intent.getStringExtra(EXTRA_DICE_TYPE);
+            diceType = DiceType.valueOf(s);
+        }
+
+        if (intent.hasExtra(EXTRA_DICE_NUMBER)){
+            diceNumber = intent.getIntExtra(EXTRA_DICE_NUMBER, 1);
+        }
     }
 
 
