07b4face157428f94290568beadff805199c92d4
Enrico <kaiden3991@gmail.com>
2017-09-12 15:31:12 +0200

    added dice images vor all options, and added the arrays to hashmap

--- app/src/main/java/strecha/com/diceroller_3d/RollActivity.java
+++ app/src/main/java/strecha/com/diceroller_3d/RollActivity.java
@@ -34,7 +34,7 @@ public class RollActivity extends AppCompatActivity implements SensorEventListen
 
     //diceType and diceNumber initialized with Default Values
     private DiceType diceType = DiceType.D6;
-    private int diceNumber = 10;
+    private int diceNumber = 1;
     private Settings settings;
     private ArrayList<Integer> history;
 
@@ -77,7 +77,13 @@ public class RollActivity extends AppCompatActivity implements SensorEventListen
         res = getResources();
         history = new ArrayList<>();
         diceImagesMap = new HashMap<>();
+        diceImagesMap.put(DiceType.D2, new int[] { R.drawable.d2_1, R.drawable.d2_2 });
+        diceImagesMap.put(DiceType.D4, new int[] { R.drawable.d4_1, R.drawable.d4_2, R.drawable.d4_3, R.drawable.d4_4 });
         diceImagesMap.put(DiceType.D6, new int[] { R.drawable.d6_1, R.drawable.d6_2, R.drawable.d6_3, R.drawable.d6_4, R.drawable.d6_5, R.drawable.d6_6 });
+        diceImagesMap.put(DiceType.D8, new int[] { R.drawable.d8_1, R.drawable.d8_2, R.drawable.d8_3, R.drawable.d8_4, R.drawable.d8_5, R.drawable.d8_6, R.drawable.d8_7, R.drawable.d8_8 });
+        diceImagesMap.put(DiceType.D10, new int[] { R.drawable.d10_1, R.drawable.d10_2, R.drawable.d10_3, R.drawable.d10_4, R.drawable.d10_5, R.drawable.d10_6, R.drawable.d10_7, R.drawable.d10_8, R.drawable.d10_9, R.drawable.d10_10 });
+        diceImagesMap.put(DiceType.D12, new int[] { R.drawable.d12_1, R.drawable.d12_2, R.drawable.d12_3, R.drawable.d12_4, R.drawable.d12_5, R.drawable.d12_6, R.drawable.d12_7, R.drawable.d12_8, R.drawable.d12_9, R.drawable.d12_10, R.drawable.d12_11, R.drawable.d12_12 });
+        diceImagesMap.put(DiceType.D20, new int[] { R.drawable.d20_1, R.drawable.d20_2, R.drawable.d20_3, R.drawable.d20_4, R.drawable.d20_5, R.drawable.d20_6, R.drawable.d20_7, R.drawable.d20_8, R.drawable.d20_9, R.drawable.d20_10, R.drawable.d20_11, R.drawable.d20_12,R.drawable.d20_13, R.drawable.d20_14, R.drawable.d20_15, R.drawable.d20_16, R.drawable.d20_17, R.drawable.d20_18, R.drawable.d20_19, R.drawable.d20_20 });
 
         // check for intent extra values
         Intent intent = getIntent();
