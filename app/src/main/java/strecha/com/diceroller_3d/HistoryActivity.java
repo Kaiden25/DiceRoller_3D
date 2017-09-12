ee67e9410bb0e3d47fca5b0eb8603409dede05ae
Lukas Streckeisen <lukas.streckeisen@hispeed.ch>
2017-09-12 16:09:34 +0200

    added history logic

--- app/src/main/java/strecha/com/diceroller_3d/HistoryActivity.java
+++ app/src/main/java/strecha/com/diceroller_3d/HistoryActivity.java
@@ -1,7 +1,12 @@
 package strecha.com.diceroller_3d;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        Intent intent = getIntent();
+        ArrayList<Integer> history = intent.getIntegerArrayListExtra(RollActivity.EXTRA_HISTORY);
+        GridLayout historyGrid = (GridLayout) findViewById(R.id.historyGrid);
+
+        for (Integer i: history){
+            Button b = new Button(this);
+            b.setText(String.valueOf(i));
+            assert historyGrid != null;
+            historyGrid.addView(b);
+        }
     }
 }
